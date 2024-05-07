9408b07c971c098ba1e4f5e6c0076447c4e39380
sipadl <fadel.mm01@gmail.com>
2024-05-07 10:37:16 +0700

    p

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'pbb',
+	'database' => 'pbb2',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
